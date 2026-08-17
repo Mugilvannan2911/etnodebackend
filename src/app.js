985977f510c489d4ef64ed32f2e175e98b8a94a4
MugilvannanRts <mugilvannanrts@gmail.com>
2026-08-17 15:00:50 +0530

    debug vercel database environment

--- src/app.js
+++ src/app.js
@@ -12,6 +12,15 @@ app.get("/", (req, res) => {
     res.send("Express server is running");
 });
 
+app.get("/api/debug-db", (req, res) => {
+  res.json({
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    database: process.env.DB_NAME
+  });
+});
+
 app.use('/api/expense', userRoutes);
 
 app.use('/api/users', authRoutes);
