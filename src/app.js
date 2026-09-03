c86a4711cbb7a6e09ed8964ebf27cd3666ef3352
MugilvannanRts <mugilvannanrts@gmail.com>
2026-09-03 12:08:59 +0530

    user auth changes

--- src/app.js
+++ src/app.js
@@ -8,9 +8,6 @@ const cookieParser = require("cookie-parser");
 
 const app = express();
 
-app.use(cookieParser());
-
-
 // CORS
 
 app.use(cors());
@@ -25,6 +22,8 @@ app.use(cors());
 // }));
 
 app.use(express.json());
+
+app.use(cookieParser());
 
 app.get("/", (req, res) => {
     res.send("Express server is running");
